31f4a677b0f72b4286ecbaa57bef1e000dc0660e
Neung0 <smddud91@gmail.com>
2018-03-10 07:28:48 +0000

    first commit

---
+++ db.py
@@ -0,0 +1,23 @@
+﻿import sqlite3
+import datetime
+from news import crawl
+
+# Connection 객체 만들기
+con = sqlite3.connect('news.db')
+# 커서 객체 만들기
+cur = con.cursor()
+
+# SQL 실행하기
+news = crawl();
+cur.execute('create table wOffice (office text, title text);')
+cur.executemany('insert into wOffice values(?, ?)', news);
+
+now = datetime.datetime.now()
+date = now.strftime('%m-%d')
+
+# 저장하기
+con.commit()
+# 연결 끊기
+con.close()
+
+# zetcode.com
---
+++ news.py
@@ -0,0 +1,42 @@
+from urllib.request import urlopen
+import datetime
+import re
+
+def find(RE, c):
+    p = re.compile(RE)
+    m = p.findall(c)
+
+    return m
+
+def dt():
+    now = datetime.datetime.now()
+    date = now.strftime('%m-%d')
+    return date
+
+def crawl():
+    url = 'http://news.naver.com/main/home.nhn#'
+    f = urlopen(url)
+    b = f.read()
+    c = b.decode('cp949')
+    f.close()
+
+    office = find('"officeName" : "(.*)"', c)
+    title = find('"title" : "(.*)"', c)
+
+    date = dt()
+    num = 0
+    news = []
+
+    for i in office:
+            if title[num] == "":
+                pass
+            else: 
+                n = [];
+                n.append(i);
+                if('\\' in title[num]):
+                    title[num] = title[num].replace('\\', '');
+                n.append(title[num]);
+                news.append(n);
+            num+=1
+    return news;
+
