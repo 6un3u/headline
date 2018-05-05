9054a57d2932698a9047faee9701aca94e5fe115
Neung0 <smddud91@gmail.com>
2018-05-05 12:45:59 +0000

    get date when user requests

--- headline.py
+++ headline.py
@@ -2,12 +2,11 @@ import sqlite3
 from bottle import route, run, template, static_file
 from datetime import date
 
-d = date.today()
-date = d.isoformat()
-date = d.strftime('%Y-%m-%d')
-
 @route('/')
 def index():
+    d = date.today()
+    date = d.isoformat()
+    date = d.strftime('%Y-%m-%d')
     con = sqlite3.connect('news.db')
     c = con.cursor()
     c.execute("select * from headline where day='%s'" % date)
