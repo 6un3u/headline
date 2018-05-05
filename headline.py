301f476b819dc1bae31c1acf56d620bec0ee5618
Neung0 <smddud91@gmail.com>
2018-05-05 13:38:10 +0000

    edit import about date

--- headline.py
+++ headline.py
@@ -1,11 +1,9 @@
 import sqlite3
 from bottle import route, run, template, static_file
-from datetime import date
-
+import datetime
 @route('/')
 def index():
-    d = date.today()
-    date = d.isoformat()
+    d = datetime.date.today()
     date = d.strftime('%Y-%m-%d')
     con = sqlite3.connect('news.db')
     c = con.cursor()
